8b55b6cc7d1244b7ac1cbdb036877d31342ed52f
rzjprogramador <rzjprogramador@gmail.com>
2021-01-16 18:12:48 -0300

    Struct dentro de struct aninhamento de estruturas

--- fundamentos/6-structs/structs.go
+++ fundamentos/6-structs/structs.go
@@ -3,23 +3,33 @@ package main
 import "fmt"
 
 type usuario struct {
-	nome  string
-	idade uint8
+	nome     string
+	idade    uint8
+	endereco endereco
+}
+
+type endereco struct {
+	longradouro    string
+	cep            string
+	numeroEndereco uint8
 }
 
 func main() {
 	var u usuario
 	fmt.Println(u)
 
-	// forma 1 de Popular uma instancia do struct usuario
+	// Instanciando struct usuario e recuperando poucos campos
 	u.nome = "Reinaldo"
 	u.idade = 43
 	fmt.Println(u)
 
-	// forma 2 de popular Instancia + recomendada
+	// forma 2 de popular Instancia -- requer recuperar todos campos
+	// Criado o struct endereco e assinalado no struct usuario
+	// para recupera-lo criar um variavel para receber os desse struct adicionado e preencher seus valores
+	enderecoExemplo := endereco{"Rua do Programador", "08474000", 72}
 
-	usuario2 := usuario{"Gustavo", 10}
-	fmt.Println(usuario2)
+	u2 := usuario{"Gustavo", 10, enderecoExemplo}
+	fmt.Println(u2)
 
 	// Recuperar somente os campos que desejar :: coloca-se só a chave e o valor dos campos que desejar
 	usuario3 := usuario{nome: "Leonardo"}
