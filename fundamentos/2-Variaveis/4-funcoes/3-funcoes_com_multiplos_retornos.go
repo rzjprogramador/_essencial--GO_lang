222da596503b101de2a72ea77e4a976349519a3b
rzjprogramador <rzjprogramador@gmail.com>
2021-01-16 00:36:19 -0300

    Ignorando ou Escolhendo Parametros a serem usados

--- fundamentos/2-Variaveis/4-funcoes/3-funcoes_com_multiplos_retornos.go
+++ fundamentos/2-Variaveis/4-funcoes/3-funcoes_com_multiplos_retornos.go
@@ -11,6 +11,11 @@ func calculosMatematicos(n1, n2 int8) (int8, int8) {
 func main() {
 	resultSoma, resultSub := calculosMatematicos(10, 15)
 	fmt.Println(resultSoma, resultSub)
+
+	// IGNORANDO ou ESCOLHENDO PARAMETROS A SEREM USADOS >>
+	//  _, resultSub := calculosMatematicos(10, 15)
+	// fmt.Println(resultSub)
+
 }
 
 /*
@@ -25,4 +30,7 @@ Rodando na funcao main esta funcao para ter os resultados :::
 criando 2 variaveis para receber por atribuição curta a funcao calculos que traz nos aprametros os numeros que serao operados
 printar na tela as variaveis que receberam essas atribuições da funcao
 
+- SE NAO QUISER USAR UM PARAMETRO Q VEM NA FUNÇÃO COLOCAR _ Handerline para ignorar um parametro
+-Obs se for ignorar cuidado com a ordem dos fatores a Ordem de cada parametro é alinhada com o tipo que ele vai retornar
+- Ignore com _ exatamente o parametro certo que nao quer usar no retorno
 */
