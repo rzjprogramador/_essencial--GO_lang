8ea52b646dd7ed0684cececc48809af3ade02965
rzjprogramador <rzjprogramador@gmail.com>
2021-01-17 00:39:20 -0300

    Slices inovação para Coleções no lugar do array

--- fundamentos/9-Colecoes--Arrays_e_Slices/Colecoes--Aarrays_e_slices.go
+++ fundamentos/9-Colecoes--Arrays_e_Slices/Colecoes--Aarrays_e_slices.go
@@ -20,34 +20,65 @@ func main() {
 	array1 := [5]string{"Posicao1", "Posicao2", "Posicao3", "Posicao4", "Posicao5"}
 	fmt.Println(array1)
 
-	// retirando uma fatia de um array com slice que a fatia da posicao 1 ate a 3 ---vai peagr a 0, 1 ,2 menos a 3
+	// retirando uma fatia de um array com slice que a fatia da posicao 1 ate a 3 ---vai pegar a 0, 1 ,2 menos a 3
 	slice2 := array1[0:3]
 	fmt.Println(slice2)
 
 	// Alterando item do array e refletindo no slice que esta pegando a fatia deste array alterado
 	array1[0] = "Posicao alterada"
 	fmt.Println(slice2)
+
+	//============================================
+	/*ARRAYS INTERNOS <<>> COMO É CONSTRUIDO O SLICE :
+	nomeSlice := make([])executar um array -tipo, quantidade-inicial, capacidade
+
+	VC NEM PRECISA PASSA A CAPACIDADE ELE JA ADEQUA PROPORCIONALMENTE PRA VC
+	DE ACORDO COM O QUE VC PASSA NA QUANTIDADE
+	O GO ADEQUA A QUANTIDADE COM O QUE VC PASSA E DOBRA A CAPACIDADE PROPORCIONAL SE BASEANDO NA QUANTIDADE Q VC PASSOU
+	ex: se vc passou QUE VAI USAR 6 POSICOES O GO TE DA 6 POSIÇÕES E 12 DE CAPACIDADE
+	ELE CRIA UM NOVO SLICE E DOBRA A CAPACIDADE DO ARRAY PARA VC
+	POR ISSO QUE O SLICE NUNCA ACABA A SUA CAPACIDADE
+
+	OBS
+
+	*/
+
+	slice3 := make([]float32, 10, 15)
+	fmt.Println(slice3)
+
+	fmt.Println(len(slice3)) // len == length -- total usado
+	fmt.Println(cap(slice3)) // cap == capacidade
+
+	//------------------------------
+	fmt.Println("=============================")
+	slice4 := make([]float32, 5) // aqui defini que tera 5 posicoes
+	fmt.Println(slice4)          // aqui mostrou as 5 posicoes com valor inicial 0
+
+	slice4 = append(slice4, 9) // adicionei com append( no slice4 um item que é o numero 200)
+	fmt.Println(slice4)        // mostrou o slice com uma posicao a mais e o que foi adicionado
+
+	fmt.Println(len(slice4)) // mostra o tamanho usado no slice
+	fmt.Println(cap(slice4)) // mostra a capacidade agora do slice que foi dobrada proporcionalmente
+
 }
 
 /*
 SLICE == FATIA DE UM ARRAY
 ele aponta para um array
 
-Slice vc nao espefica o tamanho , aceita adicionar mais valores,
+Slice vc nao espefica o tamanho , ele aceita adicionar mais valores,
 Os tipos tem que ser os mesmos
 */
 
 /*
-Array tipos de dados e tamnho são fixos
+ARRAY DEPRECIADO NO GO -- PROCURE NAO UTILIZAR --É LIMITADO EM TIPOS E QUANTIDADE DE CAPACIDADE
+SUPERADO POR SLICE
+
+Sabendo mais sobre arrays ===
+Array tipos de dados e tamanho são fixos
 vc tem que especificar o tamanho e os tipos que tem que ser do mesmo tipo
 Vc nao consegue adcionar mais items acima do que esta declarado
 Senao especificar o Tamnho ele vira um slice
 Pouco usado só se precisar espeficiar o tamanho dele na app
 
-FUNÇÕES DE COLEÇÕES
-reflect.
-TypeOf função que mostra o tipo de uma variavel
-
-append  == acrescentar
-acrescentar items ao slice
 */
--- pacotes-utilidadesGO.go
+++ pacotes-utilidadesGO.go
@@ -1,23 +1,24 @@
 /*
-FUNÇÕES DE COLEÇÕES
-======================================
+# TIPOS ::
+NUMERO     == int (int8, int16, int32, int64) de 8 em 8 bytes
+DECIMAL    == float (float8, float16, float32, float64) de 8 em 8 bytes
+TEXTO      == string
+CARACTERE  == string (Obs; Não tem char caractere unico no GO)
 
-reflect.TypeOf
-função que mostra o tipo de uma variavel
-Usando ::
-// Analisando qual o tipo deste slice
-	fmt.Println(reflect.TypeOf(slice))
-	//resp: []int  --é um slice de int
+==============================
 
-	====================================
-append  == acrescentar
-acrescentar items ao slice
+# FUNÇÕES DE COLEÇÕES
 
-Usando :
-slice = append(slice, 123)
-	fmt.Println(slice)
+> reflect.TypeOf(<varSlice>) ::: mostra o tipo de uma variavel
+> append(<varSlice>)  ::: acrescentar items ao slice
+
 
 	====================================
+# FUNÇÕES CRIADORAS
+função make -- aloca um espaço na memoria para criar algo
+DECLARAÇÃO COMO É CONSTRUIDO O SLICE :	nomeSlice := make([])<<tipo>>, <quantidade-inicial>, <capacidade>
+
+==============================
 
 // %v  para saber valor , SEPARAR ASPAS DA VAR COM VIRGULA e coloca a variavel depois das aspas
 // %T para mostrar o tipo e a var que quer saber o tipo depois das aspas tbm
@@ -25,13 +26,6 @@ slice = append(slice, 123)
 // \n -- para pular linha.
 
 ==============================
-TIPOS ::
-NUMERO     == int (int8, int16, int32, int64) de 8 em 8 bytes
-DECIMAL    == float (float8, float16, float32, float64) de 8 em 8 bytes
-TEXTO      == string
-CARACTERE  == string (Obs; Não tem char caractere unico no GO)
-
-
 
 
 */
