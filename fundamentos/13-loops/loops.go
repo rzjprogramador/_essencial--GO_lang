1c814179338b86248c1381fba88a2fa19f4e9703
rzjprogramador <rzjprogramador@gmail.com>
2021-01-18 03:28:31 -0300

    for: Repetidor

--- fundamentos/13-loops/loops.go
+++ fundamentos/13-loops/loops.go
@@ -12,19 +12,20 @@ func main() {
 		time.Sleep(time.Second)
 	}
 
-	/*
-			Conceito :
-			for interador := 0; <condicao<interador < 6; <faça interador ++ icremenatr 1 a cada interacao >>>
-			Montagem for:
-		for>enquanto varValor := 0; varValor < 6; varValor ++
+	/*for -- REPETIDOR
+
+		Conceito :
+		for interador := 0; <condicao<interador < 6; <faça interador ++ icremenatr 1 a cada interacao >>>
+		Montagem for:
+	for>enquanto varValor := 0; varValor < 6; varValor ++
 
 
-				Declarando o valor da var interadora no escopo do for
-				Obs essa variavel declarada no for só fica visivel ai neste escopo
-				não da para acessa-la fora do escopo do for
+			Declarando o valor da var interadora no escopo do for
+			Obs essa variavel declarada no for só fica visivel ai neste escopo
+			não da para acessa-la fora do escopo do for
 
-				// incrementando de 1 em 1 usar no encrementador : ++
-				// incrementando de 2 em 2 usar no encrementador : += 2
+			// incrementando de 1 em 1 usar no encrementador : ++
+			// incrementando de 2 em 2 usar no encrementador : += 2
 
 
 
